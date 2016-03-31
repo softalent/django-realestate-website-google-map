a9a0c72151341d202f0768a6287a932fb26fb73a
Brian Girer <brian.girer@gmail.com>
2016-03-31 16:19:23 -0700

    Added static root

--- realestate/realestate/settings.py
+++ realestate/realestate/settings.py
@@ -92,5 +92,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
 MEDIA_URL='...../images/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
