c4261e13d6cd3f7719ccfb853dc848f4f533acac
Brian Girer <brian.girer@gmail.com>
2016-03-31 17:04:03 -0700

    Edits to wsgi.py

--- realestate/realestate/wsgi.py
+++ realestate/realestate/wsgi.py
@@ -8,6 +8,13 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = "/root/seethisproperty/stp-django/realestate/realestate"
+    if path not in sys.path:
+        sys.path.append(path)
+        
+os.environ['PYTHON_EGG_CACHE'] = '/tmp/.python-eggs'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realestate.settings")
 
 from django.core.wsgi import get_wsgi_application
