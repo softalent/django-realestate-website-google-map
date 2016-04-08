0f69a1f0e8c0f75e2cb61207288166a14220db6a
Brian Girer <brian.girer@gmail.com>
2016-04-08 13:53:44 -0700

    more typos!

--- realestate/realestate/urls.py
+++ realestate/realestate/urls.py
@@ -25,7 +25,7 @@ urlpatterns = patterns('',
     url(r'^$', 'realestate.views.home', name='home'),
     url(r'^([A-Z]{2})/([-A-Za-z]{2,20})/([-\w\s]{3,100})/$', 'realestate.views.property', name='property'),
     #url(r'^2/ca/buffalo/address/$', 'realestate.views.test', name='test'),
-    url(r'^contact/$', 'realestate.views.send_email', name='send_email')
+    url(r'^contact/$', 'realestate.views.send_email', name='send_email'),
     # api routing
     url(r'^api/', include(router.urls)),
     url(r'^data/', include('rest_framework.urls', namespace='rest_framework')),
