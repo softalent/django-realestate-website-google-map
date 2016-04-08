e4aed2e9af1d3c33fd8336f785779dab42a60e86
Brian Girer <brian.girer@gmail.com>
2016-04-08 11:09:06 -0700

    Testing out djangorestframework

--- realestate/realestate/settings.py
+++ realestate/realestate/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'realestate',
+    'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -73,6 +74,14 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
+}
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
 
 # Internationalization
--- realestate/realestate/urls.py
+++ realestate/realestate/urls.py
@@ -1,11 +1,30 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
 
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = patterns('',
-    # Examples:
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'realestate.views.home', name='home'),
     url(r'^([A-Z]{2})/([-A-Za-z]{2,20})/([-\w\s]{3,100})/$', 'realestate.views.property', name='property'),
     #url(r'^2/ca/buffalo/address/$', 'realestate.views.test', name='test'),
+    url(r'^', include(router.urls)),
+    url(r'^data/', include('rest_framework.urls', namespace='rest_framework'))
 )
