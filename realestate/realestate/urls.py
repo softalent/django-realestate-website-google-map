a2de870a42bd44c2ab041865394c3e02f86a947b
Brian Girer <brian.girer@gmail.com>
2016-04-26 15:11:54 -0700

    working on sitemap

--- realestate/realestate/urls.py
+++ realestate/realestate/urls.py
@@ -3,7 +3,18 @@ from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from rest_framework import routers
 from realestate import views
+from django.contrib.sitemaps import GenericSitemap
+from realestate.views import PropertyListView, CityListView
 
+prop_dict = {
+    'queryset': PropertyListView,
+    'date_field': 'pub_date',
+}
+
+city_dict = {
+    'queryset': CityListView,
+    'date_field': 'pub_date',
+}
 
 router = routers.DefaultRouter()
 router.register(r'main', views.MainViewSet)
@@ -18,8 +29,9 @@ urlpatterns = patterns(
     url(r'^about/$', views.AboutView.as_view(), name='about'),
     url(r'^terms/$', views.TermsView.as_view(), name='terms'),
     url(r'^privacy/$', views.PrivacyView.as_view(), name='privacy'),
-    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
-    name='django.contrib.sitemaps.views.sitemap'),
+    url(r'^sitemap\.xml$', sitemap,
+        {'sitemaps': {'state': GenericSitemap(city_dict, priority=0.6), 'city':GenericSitemap(prop_dict, priority=0.6)}},
+        name='django.contrib.sitemaps.views.sitemap'),
     url(r'^(?P<s>\w{2})/(?P<c>\w*(-\w*)*?)/$',
         views.PropertyListView.as_view(), name='property_list'),
     url(r'^(?P<s>\w{2})/$',
