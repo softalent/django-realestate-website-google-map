1b85408e4f195e19cc658422ff342984d4fb1c57
Brian Girer <brian.girer@gmail.com>
2016-05-19 12:06:50 -0700

    PropertyFeed created with prop details in desc

--- realestate/realestate/feeds.py
+++ realestate/realestate/feeds.py
@@ -1,11 +1,13 @@
 from django.contrib.syndication.views import Feed
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_list_or_404, get_object_or_404
 from realestate import models
+import collections
 
 class PropertyFeed(Feed):
     title = 'Properties'
-    link = '/CA/Los-Angeles'
-    description = 'Properties in Los Angeles'
+    link = '/state/city'
+    description = 'Properties in requested city'
+    description_template = 'feeds/property_feed.html'
     def get_object(self, request, *args, **kwargs):
         return get_list_or_404(
             models.Main,
@@ -13,19 +15,36 @@ class PropertyFeed(Feed):
             city=kwargs.get('c', '').replace('-', ' '))
 
     def items(self, obj):
-        return obj[:30]
+        return obj
+
+    def get_item_images(self, item):
+        return item.get_images()
 
     def item_title(self, item):
         return "%s %s, %s" % (item.address, item.city, item.state)
 
-    def item_description(self, item):
-        return "%s" % item.description
+    def convert(self, data):
+        if isinstance(data, basestring):
+            return str(data)
+        elif isinstance(data, collections.Mapping):
+            return dict(map(self.convert, data.iteritems()))
+        elif isinstance(data, collections.Iterable):
+            return type(data)(map(self.convert, data))
+        else:
+            return data
 
-
-
-    # def link(self, object_list):
-    #     return object_list.get_url()
-
+    def get_context_data(self, item, **kwargs):
+        propFeatures = self.convert(item.features)
+        images = self.get_item_images(item)
+        context = super(PropertyFeed, self).get_context_data(**kwargs)
+        context['property_description'] = item.description
+        context['property_address'] = item.address
+        context['property_city'] = item.city
+        context['property_state'] = item.state
+        context['property_zip_code'] = item.zip_code
+        context['property_image'] = images[0].url
+        context['property_features'] = propFeatures
+        return context
 
 # class CityFeed(generic.ListView):
 #     def get_queryset(self):
